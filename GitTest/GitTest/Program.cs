59e3882b499eec86dadbeff06b433f863743d6b5
Hoehn, Veit {DXRE~Penzberg} <veit.hoehn@roche.com>
2018-12-06 17:44:38 +0100

    Initial commit

---
+++ GitTest/GitTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitTest.Services;
+
+namespace GitTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            INameService nameService = new NameService();
+            Console.Write(nameService.GetName());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GitTest/GitTest/Services/NameService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest.Services
+{
+    public class NameService : INameService
+    {
+        public string GetName()
+        {
+            return "Some name";
+        }
+    }
+}
